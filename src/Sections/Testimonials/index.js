a41b840f577dd64cbd1c1a496fa01e152db4b800
Daniyalamir <udanial555@gmail.com>
2023-12-09 21:15:17 -0800

    Few changes added

--- src/Sections/Testimonials/index.js
+++ src/Sections/Testimonials/index.js
@@ -103,8 +103,8 @@ const Testimonials = () => {
           />
 
           <Card
-            text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
-            name="tehreem"
+            text="Lorem Ipsum is simply dummy text ,"
+            name="Tehreem Fatima"
             image="avatar-4"
           />
         </Slider>
